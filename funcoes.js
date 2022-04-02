ad808562d71cc7a9326f59b55af48d48cdcf75c1
Milena Marlim <milenamarlim.dev@gmail.com>
2022-04-02 11:33:15 -0300

    Commit com a construção do botão darkmode

--- funcoes.js
+++ funcoes.js
@@ -1,5 +1,11 @@
+const btn = document.querySelector('.btn');
+const card = document.querySelector('.card');
+const cabecalho = document.querySelector('.cabecalho');
+const settings = document.querySelector('.settings');
 
-const botaoDarkMode= document.querySelector('.botao_darkmode')
-function clicar(){
-    botaoDarkMode.classList.toggle('ativo')
+btn.onclick = function(){
+    this.classList.toggle('ativo')
+    card.classList.toggle('ativo')
+    cabecalho.classList.toggle('ativo')
+    settings.classList.toggle('ativo')
 }
